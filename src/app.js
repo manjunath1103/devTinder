6b650dcae7599aa707d93688443f9e5f6ad5b6d1
manjunath1103 <138103367+manjunath1103@users.noreply.github.com>
2024-11-23 17:32:29 +0530

    Performed Database CRUD operations

--- src/app.js
+++ src/app.js
@@ -2,24 +2,78 @@ const express = require("express");
 const { connectDB } = require('./config/database')
 const app = express()
 
-const User = require('./models/user.js')
+app.use(express.json())
+
+const User = require('./models/user.js'); 
 
 app.post('/signup', async (req, res) => {
     try {
-        const newUser = {
-            firstName : "Manjunath",
-            lastName : "Gowda",
-            emailId : "manjunath@gmail.com",
-            password : "pass123",
-            age : 21,
-            gender : "Male"
-        }
+        const newUser = req.body
         const user = new User(newUser);
         await user.save()
         res.send(user)
     } catch (err) {
         res.status(400).send("Error saving user")
+    } 
+})
+
+// GET - User by emailId
+app.get('/user', async (req, res) => {
+    try {
+        const {emailId} = req.body
+        const users = await User.findOne({emailId})
+        if(users.length === 0) {
+            res.status(404).send('User not found')
+        }else{
+            res.send(users)
+        }
+    } catch (err) {
+        res.status(400).send(err)
     }
+})
+
+// GET - All users info for feed
+app.get('/feed', async (req, res) => {
+    try {
+        const users = await User.find({})
+        res.send(users)
+    } catch (err) {
+        res.status.send("Something went wrong")
+    }
+})
+
+// DELETE - By emailId
+app.delete('/user', async (req, res) => {
+    try {
+        const {emailId} = req.body
+        // const userDeleted = await User.findOneAndDelete({emailId})
+        // return res.send(userDeleted)
+        const user = await User.findOne({emailId})
+        if(user) {
+            const id = user._id
+            const deletedUser = await User.findByIdAndDelete(id)
+            res.send(deletedUser)
+        }else{
+            res.status(404).send("User not found")
+        }
+    } catch (err) {
+        res.status(400).send("Something went wrong")
+    }
+})
+
+app.patch('/user', async (req, res) => {
+    try {
+        const data = req.body
+        const {emailId} = data
+        const updatedUser = await User.findOneAndUpdate({emailId}, data, {returnDocument: "after"})
+        res.send(updatedUser)
+    } catch (err) {
+        res.status(400).send("Something went wrong")
+    }
+})
+
+app.get("/test", (req, res) => {
+    res.send("Test from server")
 })
 
 connectDB()
@@ -29,9 +83,5 @@ connectDB()
     })
     .catch(() => {
         console.log("Database cannot be connected!")
-    })
+    }) 
 
-
-app.get("/test", (req, res) => {
-    res.send("Test from server")
-})
