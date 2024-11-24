8d88829f012b8fb1a3037debad00c4cb8ea9bb8c
manjunath1103 <138103367+manjunath1103@users.noreply.github.com>
2024-11-24 14:14:43 +0530

    API Level Data Sanitization and Validation

--- src/app.js
+++ src/app.js
@@ -4,27 +4,41 @@ const app = express()
 
 app.use(express.json())
 
-const User = require('./models/user.js'); 
+const User = require('./models/user.js');
 
 app.post('/signup', async (req, res) => {
     try {
+        const ALLOWED_DATA = ['firstName', 'lastName', 'emailId', 'password', 'age', 'gender', 'pathToUrl']
+        const REQUIRED_DATA = ['firstName', 'lastName', 'emailId', 'password', 'age', 'gender']
+
         const newUser = req.body
+        const fields = Object.keys(newUser)
+
+        if(!REQUIRED_DATA.every(field => fields.includes(field))) {
+            throw new Error("Missing Required Data");
+        }
+
+        if(!fields.every(field => ALLOWED_DATA.includes(field))) {
+            throw new Error("Invalid data for signup");
+        }
+        
         const user = new User(newUser);
         await user.save()
         res.send(user)
+
     } catch (err) {
-        res.status(400).send("Error saving user")
-    } 
+        res.status(400).send(err.message)
+    }
 })
 
 // GET - User by emailId
 app.get('/user', async (req, res) => {
     try {
-        const {emailId} = req.body
-        const users = await User.findOne({emailId})
-        if(users.length === 0) {
+        const { emailId } = req.body
+        const users = await User.findOne({ emailId })
+        if (users.length === 0) {
             res.status(404).send('User not found')
-        }else{
+        } else {
             res.send(users)
         }
     } catch (err) {
@@ -45,15 +59,15 @@ app.get('/feed', async (req, res) => {
 // DELETE - By emailId
 app.delete('/user', async (req, res) => {
     try {
-        const {emailId} = req.body
+        const { emailId } = req.body
         // const userDeleted = await User.findOneAndDelete({emailId})
         // return res.send(userDeleted)
-        const user = await User.findOne({emailId})
-        if(user) {
+        const user = await User.findOne({ emailId })
+        if (user) {
             const id = user._id
             const deletedUser = await User.findByIdAndDelete(id)
             res.send(deletedUser)
-        }else{
+        } else {
             res.status(404).send("User not found")
         }
     } catch (err) {
@@ -61,14 +75,26 @@ app.delete('/user', async (req, res) => {
     }
 })
 
-app.patch('/user', async (req, res) => {
+app.patch('/user/:emailId', async (req, res) => {
     try {
         const data = req.body
-        const {emailId} = data
-        const updatedUser = await User.findOneAndUpdate({emailId}, data, {returnDocument: "after"})
+        const { emailId } = req.params
+        const ALLOWED_UPDATES = ['firstName', 'lastName', 'password', 'age', 'gender', 'pathToUrl', 'about', 'skills']
+
+        const canUpdate = Object.keys(data).every((field) => ALLOWED_UPDATES.includes(field))
+        if (!canUpdate) throw new Error("Invalid data to update.")
+
+        const updatedUser =
+            await User.findOneAndUpdate(
+                { emailId },
+                data,
+                { returnDocument: "after", runValidators: true }
+            )
+
         res.send(updatedUser)
+
     } catch (err) {
-        res.status(400).send("Something went wrong")
+        res.status(400).send(err.message)
     }
 })
 
@@ -83,5 +109,5 @@ connectDB()
     })
     .catch(() => {
         console.log("Database cannot be connected!")
-    }) 
+    })
 
--- src/models/user.js
+++ src/models/user.js
@@ -2,23 +2,64 @@ const mongoose = require("mongoose")
 
 const userSchema = new mongoose.Schema({
     firstName: {
-        type: String
+        type: String,
+        required: true,
+        trim: true,
+        minLength: 2,
+        maxLength: 50
     },
     lastName: {
-        type: String
+        type: String,
+        trim: true
     },
     emailId: {
-        type: String
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
     },
     password: {
-        type: String
+        type: String,
+        required: true,
+        minLength: 4,
+        maxLength: 12
     },
     age: {
-        type: Number
+        type: Number,
+        min: 15,
+        max: 75
     },
     gender: {
-        type: String
+        type: String,
+        required: true,
+        lowercase: true,
+        validate: {
+            validator: function (value) {
+                return ['male', 'female', 'other'].includes(value)
+            },
+            message: "Invalid Gender"
+        }
+    },
+    pathToUrl: {
+        type: String,
+        default: "URL to profile picture"
+    },
+    about: {
+        type: String,
+        default: "This is about section of the developer"
+    },
+    skills: {
+        type: [String],
+        validate: {
+            validator: function (value) {
+                return value.length <= 20
+            },
+            message: "Too many skills"
+        }
     }
+}, {
+    timestamps: true
 });
 
 module.exports = mongoose.model("User", userSchema)
