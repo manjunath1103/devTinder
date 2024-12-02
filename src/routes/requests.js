925a2a24cf83f91685745d8a56d2d06af4e585be
manjunath1103 <138103367+manjunath1103@users.noreply.github.com>
2024-12-03 03:08:04 +0530

    Accept or reject connection requests

--- src/routes/requests.js
+++ src/routes/requests.js
@@ -11,17 +11,17 @@ requestRouter.post('/send/:status/:toUserId', userAuth, async (req, res) => {
         const fromUserId = req.user._id
         const { toUserId, status } = req.params
 
-        if(fromUserId == toUserId) {
+        if (fromUserId == toUserId) {
             throw new Error("Cannot send request to myself");
         }
 
         const ALLOWED_STATUS = ["interested", "ignored"]
-        if(!ALLOWED_STATUS.includes(status)) {
+        if (!ALLOWED_STATUS.includes(status)) {
             throw new Error("Invalid Status Type");
         }
 
         const toUser = await User.findById(toUserId)
-        if(!toUser) {
+        if (!toUser) {
             throw new Error("User Not Found");
         }
 
@@ -46,9 +46,9 @@ requestRouter.post('/send/:status/:toUserId', userAuth, async (req, res) => {
             interested: "is interested in",
             ignored: "ignored"
         };
-        
+
         const message = `${req.user.firstName} ${statusMessages[status]} ${toUser.firstName}`;
-        
+
         res.json({
             message,
             connectionRequestData
@@ -56,6 +56,43 @@ requestRouter.post('/send/:status/:toUserId', userAuth, async (req, res) => {
 
     } catch (err) {
         res.status(400).json({ message: err.message })
+    }
+})
+
+requestRouter.post('/review/:status/:requestId', userAuth, async (req, res) => {
+    try {
+
+        // Akshay => elon
+        // elon should be looged user
+        // status => interested
+        // requestId should be "accepted" or "rejected"cons
+
+        const loggedInUser = req.user
+        const { requestId, status } = req.params
+
+        const ALLOWED_STATUS = ["accepted", "rejected"]
+        if (!ALLOWED_STATUS.includes(status)) {
+            return res.status(400).json({ message: "Invalid Status Type." })
+        }
+
+        const connectionRequest = await ConnectionRequest.findOne({
+            _id: requestId,
+            toUserId: loggedInUser._id,
+            status: "interested"
+        })
+
+        if (!connectionRequest) {
+            return res.status(404).json({ message: "Connection Request Not Found" })
+        }
+
+        connectionRequest.status = status
+
+        const updatedRequest = await connectionRequest.save()
+
+        res.json({message : `Connection request ${data.status}`, updatedRequest})
+
+    } catch (err) {
+        res.status(500).json({ message: err.message })
     }
 })
 
